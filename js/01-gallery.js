6624f4237f6491bda0f60cad5ac8eb0a5f146648
Vladyslav <vladyslavromanenkoo@gmail.com>
2023-04-27 12:35:17 +0300

    first task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+// Сворення шаблону і додавання картинок
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkupItems = ({ preview, original, description }) => {
+  return `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+};
+
+const galleryMarkup = galleryItems
+  .map((item) => galleryMarkupItems(item))
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// події
+
+const handleGalleryItemClick = (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const imageSrc = event.target.getAttribute("data-source");
+  const imageAlt = event.target.getAttribute("alt");
+
+  const modalInstance = basicLightbox.create(`
+    <div class="modal-content">
+      <img src="${imageSrc}" alt="${imageAlt}" width="900" height="700" />
+    </div>
+  `);
+  modalInstance.show();
+
+  const divRef = document.querySelector(".modal-content");
+
+  divRef.addEventListener("click", () => {
+    modalInstance.close();
+  });
+
+  galleryList.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      modalInstance.close();
+    }
+  });
+};
+
+galleryList.addEventListener("click", handleGalleryItemClick);
